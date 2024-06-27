8cbffe758281ced4fe503eee02378e24bd806bca
PhilinVeselov <110721135+PhilinVeselov@users.noreply.github.com>
2024-06-27 15:47:47 +1000

    Delete kt2/project directory

--- kt2/project/client/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Запрашиваем содержимое файла с сервера
-$content = file_get_contents ("http://server:80");
-echo $content;
-?>
--- kt2/project/server/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Читаем содержимое файла text.txt
-$content = file_get_contents("./text.txt");
-echo $content;
-?>
